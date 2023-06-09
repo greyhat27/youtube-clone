e4e9ffcceb2080419202334f3bd8a9764ac21fe1
Abhishek Katore <katoreabhi27@gmail.com>
2023-06-09 11:40:50 +0530

    first commit

---
+++ src/context/contextApi.js
@@ -0,0 +1,41 @@
+import React, { createContext, useState, useEffect } from "react";
+import { fetchData } from "../utils/api";
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState(false);
+  const [selectCategories, setSelectCategories] = useState("New");
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  useEffect(() => {
+    fetchSelectedCategoryData(selectCategories);
+  }, [selectCategories]);
+
+  const fetchSelectedCategoryData = (query) => {
+    setLoading(true);
+    fetchData(`search/?q=${query}`).then(({ contents }) => {
+      console.log("response", contents);
+      setSearchResults(contents);
+      setLoading(false);
+    });
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        loading,
+        setLoading,
+        searchResults,
+        setSearchResults,
+        selectCategories,
+        setSelectCategories,
+        mobileMenu,
+        setMobileMenu,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+};
---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: {
+    hl: "en",
+    gl: "US",
+  },
+  headers: {
+    "X-RapidAPI-Key": "e874fa9db5mshce6ffbba2a09d2ap162befjsn7c7c0c737a3b",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
